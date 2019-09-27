be90efef6e7abf11e6bd97fdd45c1cb1bd958823
ZhaoxinLee <43050503+ZhaoxinLee@users.noreply.github.com>
2019-09-27 11:21:14 -0400

    Add files via upload

--- pycrafter4500_stepper-based-master/client side/vision.py
+++ pycrafter4500_stepper-based-master/client side/vision.py
@@ -1,1 +1,13 @@
-import numpy
+import cv2, time
+
+while True:
+    video = cv2.VideoCapture(0)
+    check, frame = video.read()
+    cv2.imshow("Capturing",frame)
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+
+video.release()
