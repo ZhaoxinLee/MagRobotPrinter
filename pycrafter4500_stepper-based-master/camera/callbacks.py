054bdcbd5ac43fedff6c06ec3159d56cf4ae5778
ZhaoxinLee <43050503+ZhaoxinLee@users.noreply.github.com>
2020-01-03 16:17:00 -0500

    Add files via upload

---
+++ pycrafter4500_stepper-based-master/camera/callbacks.py
@@ -0,0 +1,87 @@
+﻿import math
+import time
+from vision import Vision
+from PyQt5 import uic
+from PyQt5.QtCore import QFile, QRegExp, QTimer
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow, QMenu, QMessageBox,QTableWidgetItem
+#=========================================================
+# a class that handles the signal and callbacks of the GUI
+#=========================================================
+# UI config
+qtCreatorFile = "mainwindow.ui"
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+vision = Vision(mode='Video')
+# vision = Vision(mode='Camera')
+#=========================================================
+# a class that handles the signal and callbacks of the GUI
+#=========================================================
+
+class GUI(QMainWindow,Ui_MainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        # self.updateRate = int(round(1000/vision.getFrameRate()))
+        self.updateRate = 15
+        self.setupUi(self)
+
+        self.setupFileMenu()
+        self.setupHelpMenu()
+        self.setupObjectDetection()
+        self.setupTimer()
+
+        self.time_ms = 0
+        self.intensity = 0
+
+    def setupTimer(self):
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(self.updateRate) # msec
+
+    def update(self):
+        vision.updateFrame()
+
+    def setupObjectDetection(self):     ##
+        self.btn_original.clicked.connect(self.on_btn_runOriginal)
+        self.btn_grayscale.clicked.connect(self.on_btn_runGrayscale)
+        self.btn_binary.clicked.connect(self.on_btn_runBinary)
+        self.btn_detectContour.clicked.connect(self.on_btn_runObjectDetection)
+        self.btn_susContour.clicked.connect(self.on_btn_susObjectDetection)
+        self.btn_clearContour.clicked.connect(self.on_btn_clearObjectDetection)
+        self.btn_snapshot.clicked.connect(self.on_btn_snapshot)
+
+    def about(self):
+        QMessageBox.about(self, "About Vision",
+                "<p>For the model of <b>Camera</b>, please refer to OV2710 Webcam on Amazon. <br>" \
+                "Jason</p>")
+
+    def setupFileMenu(self):
+        fileMenu = QMenu("&File", self)
+        self.menuBar().addMenu(fileMenu)
+        fileMenu.addAction("E&xit", QApplication.instance().quit, "Ctrl+Q")
+
+    def setupHelpMenu(self):
+        helpMenu = QMenu("&Help", self)
+        self.menuBar().addMenu(helpMenu)
+        helpMenu.addAction("&About", self.about)
+
+    def on_btn_runOriginal(self):
+        vision.setOriginal()
+
+    def on_btn_runGrayscale(self):
+        vision.setGrayscale()
+
+    def on_btn_runBinary(self):
+        vision.setThreshold(self.spb_threshval.value(),self.spb_maxval.value())
+
+    def on_btn_runObjectDetection(self):
+        vision.setObjectDetection(self.spb_threshval.value(),self.spb_maxval.value())
+
+    def on_btn_susObjectDetection(self):
+        vision.susObjectDetection()
+
+    def on_btn_clearObjectDetection(self):
+        vision.setOriginal()
+
+    def on_btn_snapshot(self):
+        vision.setSnapshot()
---
+++ pycrafter4500_stepper-based-master/camera/vision.py
@@ -0,0 +1,171 @@
+import numpy as np
+import cv2, sys, os, time
+from objectDetection import drawContour
+
+class Vision(object):
+    def __init__(self,mode):
+        if mode == 'Video':
+            self.mode = mode
+            self.cap = cv2.VideoCapture("Frog.avi")
+            self.frameCounter = 0
+        else:
+            self.mode = mode
+            self.cap = cv2.VideoCapture(0)
+        cv2.namedWindow(self.windowName(),16)
+        cv2.moveWindow(self.windowName(), 0, 100)
+        cv2.resizeWindow(self.windowName(), 640,512)
+
+        self._isGrayscale = False
+        self._isThresholdRunning = False
+        self._isObjectDetectionRunning = False
+        self.threshold = None
+        self.maxval = None
+        self.cnt_routing = np.zeros((0,2),dtype=np.uint8)
+        self.state = 0
+        self.firstFrameNo = None
+        self.snapshotState = False
+
+    def updateFrame(self):
+        if self.mode == 'Video':
+            ret, frame = self.cap.read()
+
+            self.frameCounter += 1
+            #If the last frame is reached, reset the capture and the frame_counter
+            if self.frameCounter == self.cap.get(cv2.CAP_PROP_FRAME_COUNT):
+                self.frameCounter = 0 #Or whatever as long as it is the same as next line
+                self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+            if self.isObjectDetectionRunning():
+                # to prevent overflow of cnt_routing, we just capture the contour for one video cycle
+                if self.state == 0:
+                    self.firstFrameNo = self.frameCounter
+                    self.state = 1
+                # if loop back to the n-1 frame, append no more, special case for the frame 1
+                if self.firstFrameNo == 1:
+                    if self.frameCounter == self.cap.get(cv2.CAP_PROP_FRAME_COUNT):
+                        self.state = 2
+                else:
+                    if self.frameCounter == self.firstFrameNo - 1:
+                        self.state = 2
+
+                grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                filteredFrame = self.runThreshold(grayFrame)
+                processedFrame, cnts = drawContour(filteredFrame,frame)
+                if self.state == 1:
+                    self.cnt_routing = np.append(self.cnt_routing, cnts, axis=0)
+                processedFrame[self.cnt_routing[:,1],self.cnt_routing[:,0]] = (0, 255, 0)
+                cv2.imshow(self.windowName(),processedFrame)
+                shownFrame = processedFrame
+            elif self.isThresholdRunning():
+                grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                filteredFrame = self.runThreshold(grayFrame)
+                cv2.imshow(self.windowName(),filteredFrame)
+                shownFrame = filteredFrame
+            elif self.isGrayscale():
+                grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                cv2.imshow(self.windowName(),grayFrame)
+                shownFrame = grayFrame
+            else:
+                cv2.imshow(self.windowName(),frame)
+                shownFrame = frame
+            if self.snapshotState:
+                timestr = time.strftime("%Y%m%d%H%M%S")
+                cv2.imwrite(os.getcwd()+'/snapshot/snapshot_'+timestr+'.png',shownFrame)
+                self.snapshotState = False
+
+        else:
+            ret, frame = self.cap.read()
+            if self.isObjectDetectionRunning():
+                grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                filteredFrame = self.runThreshold(grayFrame)
+                processedFrame, cnts = drawContour(filteredFrame,frame)
+                if self.state == 0:
+                    self.cnt_routing = np.append(self.cnt_routing, cnts, axis=0)
+                processedFrame[self.cnt_routing[:,1],self.cnt_routing[:,0]] = (0, 255, 0)
+                cv2.imshow(self.windowName(),processedFrame)
+                shownFrame = processedFrame
+            elif self.isThresholdRunning():
+                grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                filteredFrame = self.runThreshold(grayFrame)
+                cv2.imshow(self.windowName(),filteredFrame)
+                shownFrame = filteredFrame
+            elif self.isGrayscale():
+                grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                cv2.imshow(self.windowName(),grayFrame)
+                shownFrame = grayFrame
+            else:
+                cv2.imshow(self.windowName(),frame)
+                shownFrame = frame
+            if self.snapshotState:
+                timestr = time.strftime("%Y%m%d%H%M%S")
+                cv2.imwrite(os.getcwd()+'/snapshot/snapshot_'+timestr+'.png',shownFrame)
+                self.snapshotState = False
+            # if self.isVideoWritingEnabled():
+            #     self.videoWriter.write(filterlib.color(frameProcessed))
+
+
+    def windowName(self):
+        return 'Camera'
+
+    def getFrameRate(self):
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        return(self.fps)
+
+    def isGrayscale(self):
+        return self._isGrayscale
+
+    def isThresholdRunning(self):
+        return self._isThresholdRunning
+
+    def isObjectDetectionRunning(self):
+        return self._isObjectDetectionRunning
+
+    def setOriginal(self):
+        self._isGrayscale = False
+        self._isThresholdRunning = False
+        self._isObjectDetectionRunning = False
+        self.cnt_routing = np.zeros((0,2),dtype=np.uint8)
+        self.state = 0
+        self.firstFrameNo = None
+
+    def setGrayscale(self):
+        self._isGrayscale = True
+        self._isThresholdRunning = False
+        self._isObjectDetectionRunning = False
+
+    def setThreshold(self,threshold,maxval):
+        self.threshold = threshold
+        self.maxval = maxval
+        self._isThresholdRunning = True
+        self._isObjectDetectionRunning = False
+
+    def setObjectDetection(self,threshold,maxval):
+        self.threshold = threshold
+        self.maxval = maxval
+        self._isObjectDetectionRunning = True
+
+    def susObjectDetection(self):
+        self.state = 2
+
+    def runThreshold(self,inputImage):
+        _, ret = cv2.threshold(inputImage,self.threshold,self.maxval,cv2.THRESH_BINARY)
+        return ret
+
+    def setSnapshot(self):
+        self.snapshotState = True
+
+    # #==============================================================================================
+    # # Video recording
+    # #==============================================================================================
+    # def createVideoWriter(self,fileName):
+    #     self.videoWriter = cv2.VideoWriter(fileName,fourcc=cv2.VideoWriter_fourcc(*'XVID'),fps=30.0,frameSize=(640,480),isColor=True)
+    #
+    # def startRecording(self,fileName):
+    #     self.createVideoWriter(fileName)
+    #     self.setVideoWritingEnabled(True)
+    #     print('Start recording' + fileName)
+    #
+    # def stopRecording(self):
+    #     self.setStateSnapshotEnabled(False)
+    #     self.videoWriter.release()
+    #     print('Stop recording.')
